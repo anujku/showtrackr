65d3abd26cd03fd525ac2aa5c013107a48bc4f3e
Anuj Kulkarni <anuj@ccs.neu.edu>
2016-03-05 17:36:22 -0500

    Added moongose

--- server.js
+++ server.js
@@ -2,7 +2,34 @@ var express = require('express'),
     path = require('path'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    bcrypt = require('bcryptjs');
+
+var showSchema = new mongoose.Schema({
+    _id: Number,
+    name: String,
+    airsDayOfWeek: String,
+    airsTime: String,
+    firstAired: Date,
+    genre: [String],
+    network: String,
+    overview: String,
+    rating: Number,
+    ratingCount: Number,
+    status: String,
+    poster: String,
+    subscribers: [{
+        type: mongoose.Schema.Types.ObjectId, ref: 'User'
+    }],
+    episodes: [{
+        season: Number,
+        episodeNumber: Number,
+        episodeName: String,
+        firstAired: Date,
+        overview: String
+    }]
+});
 
 var app = express();
 
